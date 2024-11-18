297cfcedaffdf9915dcacb1f0120b50898e2a5b7
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-11-18 15:26:24 +0000

    task(tooling): initial commit and barebones

---
+++ README.md
@@ -0,0 +1,5 @@
+# Planning Inspectorate Crown Developments
+
+## Licensing
+
+[MIT](https://opensource.org/licenses/mit) © Planning Inspectorate
---
+++ infrastructure/variables.tf
@@ -0,0 +1,23 @@
+# variables should be sorted A-Z
+
+variable "environment" {
+  description = "The name of the environment in which resources will be deployed"
+  type        = string
+}
+
+variable "tags" {
+  description = "A collection of tags to assign to taggable resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "tooling_config" {
+  description = "Config for the tooling subscription resources"
+  type = object({
+    container_registry_name = string
+    container_registry_rg   = string
+    network_name            = string
+    network_rg              = string
+    subscription_id         = string
+  })
+}
