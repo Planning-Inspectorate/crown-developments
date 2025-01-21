5f207443e78a4e98dbcab75c4aa3e50374d26dd3
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-01-21 17:12:30 +0000

    feat(crown-8): update createApplicationInfoRoutes function name

--- apps/portal/src/app/router.js
+++ apps/portal/src/app/router.js
@@ -1,8 +1,8 @@
 import { Router as createRouter } from 'express';
 import { viewHomepage } from './views/home/controller.js';
 import { createMonitoringRoutes } from '@pins/crowndev-lib/controllers/monitoring.js';
-import { createRoutes as applicationRoutes } from './views/applications/index.js';
-import { createInfoRoutes } from './views/application-info/index.js';
+import { createRoutes as applicationRoutes } from './views/projects/index.js';
+import { createApplicationInfoRoutes } from './views/application-info/index.js';
 
 /**
  * @param {Object} params
@@ -24,7 +24,7 @@ export function buildRouter({ logger, config, dbClient }) {
 
 	router.route('/').get(viewHomepage);
 	router.use('/', applicationRoutes({ db: dbClient, logger }));
-	router.use('/', createInfoRoutes({ db: dbClient, logger }));
+	router.use('/', createApplicationInfoRoutes({ db: dbClient, logger }));
 
 	return router;
 }
--- apps/portal/src/app/views/application-info/index.js
+++ apps/portal/src/app/views/application-info/index.js
@@ -2,7 +2,7 @@ import { Router as createRouter } from 'express';
 import { asyncHandler } from '@pins/crowndev-lib/util/async-handler.js';
 import { buildApplicationInformationPage } from './controller.js';
 
-export function createInfoRoutes(opts) {
+export function createApplicationInfoRoutes(opts) {
 	const router = createRouter({ mergeParams: true });
 	const listCases = buildApplicationInformationPage(opts);
 
