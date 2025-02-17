aa8c3e8fcec95ee0ce938057e470369f0001604d
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-02-17 14:32:25 +0000

    fix(manage): if coordinates not populated leave blank on CYA

--- apps/manage/src/app/views/cases/view/question-utils.js
+++ apps/manage/src/app/views/cases/view/question-utils.js
@@ -49,7 +49,7 @@ export function contactQuestions({ prefix, title, addressRequired }) {
 							maxLengthMessage: `${title} name must be less than 250 characters`
 						},
 						regex: {
-							regex: "^[A-Za-z0-9 '’-]+$",
+							regex: "^[A-Za-z0-9 '’-]*$",
 							regexMessage: 'Full name must only include letters, spaces, hyphens, apostrophes or numbers'
 						}
 					},
