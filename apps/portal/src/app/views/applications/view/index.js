01220f3bf6aa8b7a68a3ceb92dc639ee1e2a4889
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-02-18 10:28:09 +0000

    feat(portal): document view/download

--- apps/portal/src/app/views/applications/view/index.js
+++ apps/portal/src/app/views/applications/view/index.js
@@ -1,6 +1,6 @@
 import { Router as createRouter } from 'express';
 import { applicationInfoRoutes } from './application-info/index.js';
-import { buildApplicationDocumentsPage } from './documents/controller.js';
+import { buildApplicationDocumentsPage, buildDocumentView } from './documents/controller.js';
 import { viewHaveYourSayPage } from './have-your-say/controller.js';
 import { asyncHandler } from '@pins/crowndev-lib/util/async-handler.js';
 import { applicationListRoutes } from './list/index.js';
@@ -16,12 +16,14 @@ import { applicationListRoutes } from './list/index.js';
 export function createRoutes(opts) {
 	const router = createRouter({ mergeParams: true });
 	const applicationDocumentsPage = buildApplicationDocumentsPage(opts);
+	const viewDocumentPage = buildDocumentView(opts);
 
 	router.use('/', applicationListRoutes(opts));
 	router.use('/application-information/:applicationId', applicationInfoRoutes(opts));
 
 	// placeholders for documents and have-your-say routes
 	router.get('/application-information/:applicationId/documents', asyncHandler(applicationDocumentsPage));
+	router.get('/application-information/:applicationId/documents/:documentId', asyncHandler(viewDocumentPage));
 	router.get('/application-information/:applicationId/have-your-say', viewHaveYourSayPage);
 
 	return router;
