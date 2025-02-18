695dd4d875e957b1ddb3db64137a5eab58ccc75f
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-02-18 10:28:09 +0000

    feat(portal): document view/download

--- apps/portal/src/app/views/applications/view/documents/controller.js
+++ apps/portal/src/app/views/applications/view/documents/controller.js
@@ -5,6 +5,7 @@ import { fetchPublishedApplication } from '#util/applications.js';
 import { mapDriveItemToViewModel } from './view-model.js';
 import { applicationLinks } from '../view-model.js';
 import { sortByField } from '@pins/crowndev-lib/util/array.js';
+import { Readable } from 'stream';
 
 const PUBLISHED_FOLDER = 'Published';
 
@@ -42,6 +43,35 @@ export function buildApplicationDocumentsPage({ db, logger, sharePointDrive }) {
 			currentUrl: req.originalUrl,
 			documents
 		});
+	};
+}
+
+/**
+ * Render a document
+ * @param {Object} opts
+ * @param {import('@prisma/client').PrismaClient} opts.db
+ * @param {import('pino').BaseLogger} opts.logger
+ * @param {import('@pins/crowndev-sharepoint/src/sharepoint/drives/drives.js').SharePointDrive} opts.sharePointDrive
+ * @param {global.fetch} [opts.fetchImpl] - for testing
+ * @returns {import('express').Handler}
+ */
+export function buildDocumentView({ db, logger, sharePointDrive, fetchImpl }) {
+	return async (req, res) => {
+		const documentId = req.params?.documentId;
+		if (!documentId) {
+			throw new Error('documentId param is required');
+		}
+
+		const crownDevelopment = await checkApplicationPublished(req, res, db);
+		if (!crownDevelopment) {
+			return; // handled by checkApplicationPublished
+		}
+		const { reference } = crownDevelopment;
+
+		logger.debug({ reference, documentId }, 'download file');
+
+		const downloadUrl = await sharePointDrive.getDriveItemDownloadUrl(documentId);
+		await forwardStreamContents(downloadUrl, req, res, logger, documentId, fetchImpl);
 	};
 }
 
@@ -97,4 +127,48 @@ async function getDocuments(sharePointDrive, path, logger, id) {
 		logger.error({ err, id }, 'error fetching documents from sharepoint');
 		throw new Error('There is a problem fetching documents');
 	}
+}
+
+/**
+ * @param {string} downloadUrl
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('pino').BaseLogger} logger
+ * @param {string} documentId
+ * @param {global.fetch} [fetchImpl] - for testing
+ * @returns {Promise<void>}
+ */
+async function forwardStreamContents(downloadUrl, req, res, logger, documentId, fetchImpl = fetch) {
+	// set up an abort controller to cancel requests
+	const controller = new AbortController();
+
+	const downloadRes = await fetchImpl(downloadUrl, {
+		signal: controller.signal
+	});
+
+	// abort downloads if the incoming request is closed
+	req.on('close', () => {
+		logger.debug({ documentId }, 'request closed, aborting download');
+		controller.abort();
+	});
+
+	res.header('Content-Type', downloadRes.headers.get('Content-Type'));
+	res.header('Content-Length', downloadRes.headers.get('Content-Length'));
+	// allow PDFs to be viewed in-browser
+	// Content-disposition will set it as an attachment and force download
+	if (!downloadRes.headers.get('Content-Type')?.includes('/pdf')) {
+		res.header('Content-disposition', downloadRes.headers.get('Content-disposition'));
+	}
+
+	const downloadStream = Readable.fromWeb(downloadRes.body);
+	// make sure to catch abort errors
+	downloadStream.on('error', (err) => {
+		if (err?.name === 'AbortError') {
+			logger.debug({ documentId }, 'file download cancelled');
+		} else {
+			logger.error({ err, documentId }, 'file download stream error');
+		}
+	});
+	// forward the contents of the file to the response
+	downloadStream.pipe(res);
 }
