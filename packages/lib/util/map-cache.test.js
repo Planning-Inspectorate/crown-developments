75ec5699475546ce03f7d16d9edaa6415a197693
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-02-24 09:29:55 +0000

    refactor(lib): use date mocking

--- packages/lib/util/map-cache.test.js
+++ packages/lib/util/map-cache.test.js
@@ -3,36 +3,36 @@ import { MapCache } from './map-cache.js';
 import assert from 'node:assert';
 
 describe('MapCache', () => {
-	it('should add entries with the current date', () => {
-		let date = new Date('2025-01-30T00:00:00.000Z');
-		const getNow = () => date;
-		const cache = new MapCache(5, getNow);
+	it('should add entries with the current date', (ctx) => {
+		const now = new Date('2025-01-30T00:00:00.000Z');
+		ctx.mock.timers.enable({ apis: ['Date'], now });
+		const cache = new MapCache(5);
 
 		cache.set('id-1', true);
 
-		assert.strictEqual(cache.cache.get('id-1').updated, date);
+		assert.deepStrictEqual(cache.cache.get('id-1').updated, now);
 	});
-	it(`should return values that aren't expired`, () => {
-		let date = new Date('2025-01-30T00:00:00.000Z');
-		const getNow = () => date;
-		const cache = new MapCache(5, getNow);
+	it(`should return values that aren't expired`, (ctx) => {
+		const now = new Date('2025-01-30T00:00:00.000Z');
+		ctx.mock.timers.enable({ apis: ['Date'], now });
+		const cache = new MapCache(5);
 		cache.set('id-1', true);
 		cache.set('id-2', 'value 2');
 
 		assert.strictEqual(cache.get('id-1'), true);
 		assert.strictEqual(cache.get('id-2'), 'value 2');
 	});
-	it(`should return undefined if no entry`, () => {
-		let date = new Date('2025-01-30T00:00:00.000Z');
-		const getNow = () => date;
-		const cache = new MapCache(5, getNow);
+	it(`should return undefined if no entry`, (ctx) => {
+		const now = new Date('2025-01-30T00:00:00.000Z');
+		ctx.mock.timers.enable({ apis: ['Date'], now });
+		const cache = new MapCache(5);
 
 		assert.strictEqual(cache.get('id-1'), undefined);
 	});
-	it(`should not return expired values`, () => {
-		let date = new Date('2025-01-30T00:00:00.000Z');
-		const getNow = () => date;
-		const cache = new MapCache(5, getNow);
+	it(`should not return expired values`, (ctx) => {
+		const now = new Date('2025-01-30T00:00:00.000Z');
+		ctx.mock.timers.enable({ apis: ['Date'], now });
+		const cache = new MapCache(5);
 		cache.set('id-1', true);
 		cache.set('id-2', 'value 2');
 
@@ -40,12 +40,12 @@ describe('MapCache', () => {
 		assert.strictEqual(cache.get('id-2'), 'value 2');
 
 		// 5 minutes on, not yet expired
-		date = new Date('2025-01-30T00:05:00.000Z');
+		ctx.mock.timers.setTime(new Date('2025-01-30T00:05:00.000Z').getTime());
 		assert.strictEqual(cache.get('id-1'), true);
 		assert.strictEqual(cache.get('id-2'), 'value 2');
 
 		// 10 minutes on, now expired
-		date = new Date('2025-01-30T00:10:00.000Z');
+		ctx.mock.timers.setTime(new Date('2025-01-30T00:10:00.000Z').getTime());
 		assert.strictEqual(cache.get('id-1'), undefined);
 		assert.strictEqual(cache.get('id-2'), undefined);
 	});
