57e21921e40781b67fcd3ed4d8b5879283bc6088
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-02-28 16:35:51 +0000

    feat(portal): configure on behalf of org i work for journey

--- apps/portal/src/app/views/applications/view/have-your-say/journey.js
+++ apps/portal/src/app/views/applications/view/have-your-say/journey.js
@@ -1,7 +1,7 @@
 import { Journey } from '@pins/dynamic-forms/src/journey/journey.js';
 import { Section } from '@pins/dynamic-forms/src/section.js';
 import { questionHasAnswer } from '@pins/dynamic-forms/src/components/utils/question-has-answer.js';
-import { REPRESENTATION_SUBMITTED_FOR_ID } from '@pins/crowndev-database/src/seed/data-static.js';
+import { REPRESENTATION_SUBMITTED_FOR_ID, REPRESENTED_TYPE_ID } from '@pins/crowndev-database/src/seed/data-static.js';
 import { BOOLEAN_OPTIONS } from '@pins/dynamic-forms/src/components/boolean/question.js';
 
 export const JOURNEY_ID = 'have-your-say';
@@ -32,15 +32,29 @@ export function createJourney(questions, response, req) {
 				.addQuestion(questions.isAgentAdult)
 				.addQuestion(questions.agentFullName)
 				.withCondition((response) => questionHasAnswer(response, questions.isAgentAdult, BOOLEAN_OPTIONS.YES))
+				.startMultiQuestionCondition('group-1', (response) =>
+					questionHasAnswer(response, questions.whoRepresenting, REPRESENTED_TYPE_ID.PERSON)
+				)
 				.addQuestion(questions.areYouAgent)
 				.addQuestion(questions.fullNameOrg)
 				.withCondition((response) => questionHasAnswer(response, questions.areYouAgent, BOOLEAN_OPTIONS.YES))
+				.endMultiQuestionCondition('group-1')
 				.addQuestion(questions.agentEmail)
+				.startMultiQuestionCondition('group-2', (response) =>
+					questionHasAnswer(response, questions.whoRepresenting, REPRESENTED_TYPE_ID.ORGANISATION)
+				)
+				.addQuestion(questions.orgName)
+				.addQuestion(questions.orgRoleName)
+				.endMultiQuestionCondition('group-2')
+				.startMultiQuestionCondition('group-3', (response) =>
+					questionHasAnswer(response, questions.whoRepresenting, REPRESENTED_TYPE_ID.PERSON)
+				)
 				.addQuestion(questions.isRepresentedPersonAdult)
 				.addQuestion(questions.representedPersonFullName)
 				.withCondition((response) =>
 					questionHasAnswer(response, questions.isRepresentedPersonAdult, BOOLEAN_OPTIONS.YES)
 				)
+				.endMultiQuestionCondition('group-3')
 				.addQuestion(questions.agentTellUsAboutApplication)
 		],
 		taskListUrl: 'check-your-answers',
