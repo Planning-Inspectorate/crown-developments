eca38a44f83bd81e0b2e4d8c376d89f1a430fa85
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-03-04 09:42:47 +0000

    feat(portal): add logic for on behalf of a person have your say journey

---
+++ packages/database/src/migrations/20250227164751_additional_representation_fields/migration.sql
@@ -0,0 +1,20 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[Representation] ADD [isAgent] BIT NOT NULL CONSTRAINT [Representation_isAgent_df] DEFAULT 1,
+[isAgentAdult] BIT NOT NULL CONSTRAINT [Representation_isAgentAdult_df] DEFAULT 1;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
