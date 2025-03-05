e5eb5846542f076a825c5294010fa970c833c22e
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-03-05 10:19:53 +0000

    chore(lib): add have-your-say view-model types

---
+++ packages/lib/forms/representations/types.d.ts
@@ -0,0 +1,67 @@
+/**
+ * The view model used for have-your-say answers
+ */
+export type HaveYourSayViewModel = HaveYourSay.Common & (HaveYourSay.Myself | HaveYourSay.OnBehalfOf);
+
+export namespace HaveYourSay {
+	/**
+	 * Common have-your-say fields for all journeys
+	 */
+	export interface Common {
+		submittedForId: string;
+	}
+
+	/**
+	 * Myself have-your-say fields
+	 */
+	export interface Myself {
+		isAdult: boolean;
+		submitterFullName?: string;
+		submitterEmail: string;
+		submitterComment: string;
+	}
+
+	/**
+	 * On behalf of have-your-say fields
+	 */
+	export type OnBehalfOf = OnBehalfOfOptions & {
+		representedTypeId: string;
+		isAgentAdult: boolean;
+		agentFullName?: string;
+		agentEmail: string;
+		agentComment: string;
+	};
+
+	/**
+	 * On behalf of have-your-say journey specific field options
+	 */
+	export type OnBehalfOfOptions = OnBehalfOfPerson | OnBehalfOfOrg | OnBehalfOfOrgNotWorkFor;
+
+	/**
+	 * On behalf of a person have-your-say fields
+	 */
+	export type OnBehalfOfPerson = IsAgent & {
+		isRepresentedPersonAdult: boolean;
+		representedPersonFullName?: string;
+	};
+
+	/**
+	 * On behalf of an org have-your-say fields
+	 */
+	export interface OnBehalfOfOrg {
+		orgName: string;
+		orgRoleName: string;
+	}
+
+	/**
+	 * On behalf of an org I don't work have-your-say fields
+	 */
+	export type OnBehalfOfOrgNotWorkFor = IsAgent & {
+		orgNameRepresenting: string;
+	};
+
+	export interface IsAgent {
+		isAgent: boolean;
+		agentOrgName?: string;
+	}
+}
