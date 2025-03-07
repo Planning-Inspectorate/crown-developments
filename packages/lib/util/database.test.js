81aa7e750efbfd7dedb801c8b5d26d9991f5b156
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-03-07 11:53:05 +0000

    refactor: add wrapPrismaError util

--- packages/lib/util/database.test.js
+++ packages/lib/util/database.test.js
@@ -1,6 +1,8 @@
 import { describe, it } from 'node:test';
-import { optionalWhere } from './database.js';
+import { optionalWhere, wrapPrismaError } from './database.js';
 import assert from 'node:assert';
+import { Prisma } from '@prisma/client';
+import { mockLogger } from '../testing/mock-logger.js';
 
 describe('database', () => {
 	describe('optionalWhere', () => {
@@ -11,6 +13,64 @@ describe('database', () => {
 		it('should return where clause if id', () => {
 			const result = optionalWhere('id-1');
 			assert.deepStrictEqual(result, { id: 'id-1' });
+		});
+	});
+	describe('wrapPrismaError', () => {
+		it('should re-throw non-Prisma errors', () => {
+			const error = new Error('Some other error');
+			const logger = mockLogger();
+			assert.throws(
+				() =>
+					wrapPrismaError({
+						error,
+						logger,
+						message: 'updating case'
+					}),
+				(err) => {
+					assert.strictEqual(err, error);
+					assert.strictEqual(err.name, 'Error');
+					return true;
+				}
+			);
+		});
+		it('should not throw Prisma client errors', () => {
+			const error = new Prisma.PrismaClientKnownRequestError('E101', {
+				code: '123'
+			});
+			const logger = mockLogger();
+			assert.throws(
+				() =>
+					wrapPrismaError({
+						error,
+						logger,
+						message: 'updating case'
+					}),
+				(err) => {
+					assert.strictEqual(err instanceof Prisma.PrismaClientKnownRequestError, false);
+					assert.strictEqual(err.name, 'Error');
+					assert.match(err.message, /Error updating case/);
+					assert.match(err.message, /\(123\)/);
+					return true;
+				}
+			);
+		});
+		it('should not throw Prisma validation errors', () => {
+			const error = new Prisma.PrismaClientValidationError('E101', {});
+			const logger = mockLogger();
+			assert.throws(
+				() =>
+					wrapPrismaError({
+						error,
+						logger,
+						message: 'updating case'
+					}),
+				(err) => {
+					assert.strictEqual(err instanceof Prisma.PrismaClientValidationError, false);
+					assert.strictEqual(err.name, 'Error');
+					assert.match(err.message, /Error updating case/);
+					return true;
+				}
+			);
 		});
 	});
 });
