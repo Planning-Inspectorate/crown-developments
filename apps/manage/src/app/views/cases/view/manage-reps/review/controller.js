b6728b6ff58efb5485c8fd6aab7a56fd5770c73c
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-03-07 13:03:05 +0000

    feat(manage): review representations page

--- apps/manage/src/app/views/cases/view/manage-reps/review/controller.js
+++ apps/manage/src/app/views/cases/view/manage-reps/review/controller.js
@@ -1,11 +1,42 @@
+import { REPRESENTATION_STATUS_ID } from '@pins/crowndev-database/src/seed/data-static.js';
+import { ACCEPT_AND_REDACT } from '@pins/crowndev-lib/forms/representations/questions.js';
+import { renderRepresentation, validateParams } from '../view/controller.js';
+
 /**
- * Placeholder
  * @type {import('express').Handler}
  */
-export function reviewRep(req, res) {
-	res.render('views/cases/view/manage-reps/review/view.njk', {
-		backLink: '#',
-		pageTitle: 'Review representation',
-		baseUrl: req.baseUrl
+export async function viewRepresentationAwaitingReview(req, res) {
+	validateParams(req.params);
+
+	await renderRepresentation(req, res, {
+		accept: REPRESENTATION_STATUS_ID.ACCEPTED,
+		acceptAndRedact: ACCEPT_AND_REDACT,
+		reject: REPRESENTATION_STATUS_ID.REJECTED
 	});
+}
+
+/**
+ * Redirect between /view and /review based on requiresReview status
+ * @type {import('express').Handler}
+ */
+export function viewReviewRedirect(req, res, next) {
+	const originalUrl = req.originalUrl;
+	if (!originalUrl.startsWith('/')) {
+		// if the URL does not start with / then do not process it
+		next();
+		return undefined;
+	}
+	const requiresReview = res.locals?.journeyResponse?.answers?.requiresReview;
+	if (originalUrl.endsWith('/view')) {
+		if (requiresReview) {
+			res.redirect(originalUrl.replace('/view', '/review'));
+			return undefined;
+		}
+	} else if (originalUrl.endsWith('/review')) {
+		if (!requiresReview) {
+			res.redirect(originalUrl.replace('/review', '/view'));
+			return undefined;
+		}
+	}
+	next();
 }
