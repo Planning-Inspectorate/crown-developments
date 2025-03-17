2ef8e75668fccf6ad1ecafdcb6fff42d569f6624
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-03-17 15:26:51 +0000

    feat(manage): move success macro into views folder in lib

--- apps/manage/src/app/nunjucks.js
+++ apps/manage/src/app/nunjucks.js
@@ -14,12 +14,14 @@ export function configureNunjucks() {
 	const govukFrontendRoot = path.resolve(require.resolve('govuk-frontend'), '../..');
 	// path to packages/lib/forms folder with custom form components
 	const customFormsRoot = path.resolve(require.resolve('@pins/crowndev-lib'), '..', 'forms');
+	// path to packages/lib/forms folder with custom views
+	const customViewsRoot = path.resolve(require.resolve('@pins/crowndev-lib'), '..', 'views');
 	const appDir = path.join(config.srcDir, 'app');
 
 	// configure nunjucks
 	return nunjucks.configure(
 		// ensure nunjucks templates can use govuk-frontend components, and templates we've defined in `web/src/app`
-		[dynamicFormsRoot, govukFrontendRoot, customFormsRoot, appDir],
+		[dynamicFormsRoot, govukFrontendRoot, customFormsRoot, customViewsRoot, appDir],
 		{
 			// output with dangerous characters are escaped automatically
 			autoescape: true,
