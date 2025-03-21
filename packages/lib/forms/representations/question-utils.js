eecfdbb434a690039d5a05b3baa182b265f1a816
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-03-21 14:50:26 +0000

    fix(portal): identify Input Purpose - Autocomplete Attribute Not Present

--- packages/lib/forms/representations/question-utils.js
+++ packages/lib/forms/representations/question-utils.js
@@ -2,7 +2,7 @@ import { CUSTOM_COMPONENTS } from '../custom-components/index.js';
 import RequiredValidator from '@pins/dynamic-forms/src/validator/required-validator.js';
 import StringValidator from '@pins/dynamic-forms/src/validator/string-validator.js';
 import { COMPONENT_TYPES } from '@pins/dynamic-forms';
-import { referenceDataToRadioOptions } from '../../util/questions.js';
+import { referenceDataToRadioOptions, toKebabCase } from '../../util/questions.js';
 import { CONTACT_PREFERENCE } from '@pins/crowndev-database/src/seed/data-static.js';
 import AddressValidator from '@pins/dynamic-forms/src/validator/address-validator.js';
 
@@ -33,6 +33,7 @@ export function representationsContactQuestions({ prefix }) {
 		hint: 'We will publish this on the website along with your comments about the application.',
 		fieldName: `${prefix}FullName`,
 		url: isSubmitter(prefix) ? `agent-full-name` : `full-name`,
+		autocomplete: toKebabCase(`${prefix}FullName`),
 		validators: [
 			new RequiredValidator('Enter Full name'),
 			new StringValidator({
@@ -55,6 +56,7 @@ export function representationsContactQuestions({ prefix }) {
 		hint: 'We will use your email address to send you information about this application. We will not publish your email address.',
 		fieldName: `${prefix}Email`,
 		url: isSubmitter(prefix) ? `agent-email-address` : `email-address`,
+		autocomplete: toKebabCase(`${prefix}Email`),
 		validators: [
 			new RequiredValidator('Enter your email address'),
 			new StringValidator({
