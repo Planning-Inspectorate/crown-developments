6dba8808c1aeeb9cb8c4c3efbe1e503a74ad916c
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-03-25 11:30:01 +0000

    feat(portal): add 'not live' feature flag implementation

--- apps/portal/src/util/config-middleware.js
+++ apps/portal/src/util/config-middleware.js
@@ -1,29 +1,34 @@
 /**
  * Add configuration values to locals.
- * @type {import('express').Handler}
+ * @param {Object} params
+ * @param {import('../app/config-types.js').Config} params.config
+ * @returns {import('express').Handler}
  */
-export function addLocalsConfiguration(req, res, next) {
-	const path = req.path;
+export function addLocalsConfiguration({ config }) {
+	return (req, res, next) => {
+		const path = req.path;
 
-	const links = [
-		{
-			text: 'Home',
-			href: '/'
-		},
-		{
-			text: 'All Applications',
-			href: '/applications'
-		}
-	];
+		const links = [
+			{
+				text: 'Home',
+				href: '/'
+			},
+			{
+				text: 'All Applications',
+				href: '/applications'
+			}
+		];
 
-	res.locals.config = {
-		headerTitle: 'Find a Crown Development Application',
-		primaryNavigationLinks: links.map((l) => {
-			const link = { current: false, ...l };
-			link.current = link.href === path;
-			return link;
-		}),
-		haveYourSayServiceName: 'Have your say on a Crown Development Application'
+		res.locals.config = {
+			headerTitle: 'Find a Crown Development Application',
+			primaryNavigationLinks: links.map((l) => {
+				const link = { current: false, ...l };
+				link.current = link.href === path;
+				return link;
+			}),
+			haveYourSayServiceName: 'Have your say on a Crown Development Application',
+			isLive: config.featureFlags?.isLive
+		};
+		next();
 	};
-	next();
 }
