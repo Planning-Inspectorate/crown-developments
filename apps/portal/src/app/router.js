12dd503f1975a523206faf1330dbd036a03fbb37
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-03-26 16:06:57 +0000

    feat(portal): remove redundant index files for t&c and contact

--- apps/portal/src/app/router.js
+++ apps/portal/src/app/router.js
@@ -1,8 +1,8 @@
 import { Router as createRouter } from 'express';
 import { createMonitoringRoutes } from '@pins/crowndev-lib/controllers/monitoring.js';
 import { createRoutes as applicationRoutes } from './views/applications/index.js';
-import { createRoutes as contactRoutes } from './views/contact/index.js';
-import { createRoutes as termsAndConditionsRoutes } from './views/terms-and-conditions/index.js';
+import { buildTermsAndConditionsPage } from './views/terms-and-conditions/controller.js';
+import { buildContactUsPage } from './views/contact/controller.js';
 
 /**
  * @param {Object} params
@@ -28,8 +28,8 @@ export function buildRouter({ logger, config, dbClient, sharePointDrive }) {
 			res.redirect('/applications');
 		});
 		router.use('/', applicationRoutes({ db: dbClient, logger, config, sharePointDrive }));
-		router.use('/contact', contactRoutes());
-		router.use('/terms-and-conditions', termsAndConditionsRoutes());
+		router.use('/contact', buildContactUsPage());
+		router.use('/terms-and-conditions', buildTermsAndConditionsPage());
 	} else {
 		logger.info("Not registering application routes, feature flag 'FEATURE_FLAG_PORTAL_NOT_LIVE' is enabled");
 	}
