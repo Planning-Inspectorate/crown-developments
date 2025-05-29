451686b438524b558202a9dca5797e0f748a4db9
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-05-29 11:55:53 +0100

    feat(manage): implement doc upload for back office manage app

--- apps/manage/src/app/service.js
+++ apps/manage/src/app/service.js
@@ -56,6 +56,10 @@ export class ManageService {
 		this.notifyClient = initGovNotify(config.govNotify, logger);
 	}
 
+	get appName() {
+		return this.#config.appName;
+	}
+
 	/**
 	 * @type {import('./config-types.js').Config['auth']}
 	 */
