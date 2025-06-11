2b06378efa6b31f85cbeb9f4982d8b103a332b06
RobBroadhead30 <rob.broadhead.30@planninginspectorate.gov.uk>
2025-06-11 14:21:08 +0100

    fix(portal): text amends on detailed information page

--- apps/portal/src/app/views/static/detailed-information/controller.js
+++ apps/portal/src/app/views/static/detailed-information/controller.js
@@ -4,17 +4,17 @@ export function buildDetailedInformationPage() {
 			{
 				title: 'Procedural guidance',
 				href: 'https://www.gov.uk/government/publications/crown-development-applications-procedural-guide',
-				description: 'The roles and responsibilities of each party in the non-urgent Crown development process'
+				description: 'The roles and responsibilities of each party in the non-urgent Crown development process.'
 			},
 			{
-				title: 'Applications documents',
+				title: 'Application documents',
 				href: 'https://www.gov.uk/government/publications/planning-application-forms-templates-for-local-planning-authorities',
-				description: 'Resources for submitting a Crown development application'
+				description: 'Resources for submitting a Crown development application.'
 			},
 			{
 				title: 'Legislation information',
 				href: 'https://www.legislation.gov.uk/uksi/2025/409/contents/made',
-				description: 'View The Town and Country Planning Order 2025 to understand our legal duties'
+				description: 'View The Town and Country Planning Order 2025 to understand our legal duties.'
 			}
 		];
 		return res.render('views/static/detailed-information/view.njk', { chevrons });
