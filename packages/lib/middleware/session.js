4c186f105bab748f7e2a363a7c06fa6905d4e569
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-06-27 17:09:12 +0100

    feat(apps): add middleware to beginning of have your say to refresh session id

--- packages/lib/middleware/session.js
+++ packages/lib/middleware/session.js
@@ -1,13 +1,15 @@
 import { notFoundHandler } from './errors.js';
 
-export function resetSessionMiddleware(logger) {
+export function buildResetSessionMiddleware(logger) {
 	return (req, res, next) => {
+		// keep account info so user isn't logged out
+		const savedAccount = req.session.account;
 		req.session.regenerate((err) => {
 			if (err) {
 				logger.error('Session regeneration error:', err);
 				return notFoundHandler(req, res);
 			} else {
-				//TODO: check how this affects BO users where they have to login...
+				req.session.account = savedAccount;
 				logger.info('User session refreshed');
 				next();
 			}
