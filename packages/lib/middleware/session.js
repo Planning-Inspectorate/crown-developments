f1366d6a5543227518377f378cc6b15a571714c6
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-06-30 09:59:15 +0100

    feat(apps): add session tests

--- packages/lib/middleware/session.js
+++ packages/lib/middleware/session.js
@@ -1,13 +1,12 @@
-import { notFoundHandler } from './errors.js';
-
 export function buildResetSessionMiddleware(logger) {
 	return (req, res, next) => {
 		// keep account info so user isn't logged out
 		const savedAccount = req.session.account;
+		// regenerate session to generate new req.sessionID
 		req.session.regenerate((err) => {
 			if (err) {
 				logger.error('Session regeneration error:', err);
-				return notFoundHandler(req, res);
+				next(err);
 			} else {
 				req.session.account = savedAccount;
 				logger.info('User session refreshed');
