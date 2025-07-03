e0f1f8bfddd1dffb197e53039a0b547090eab335
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-07-03 16:29:04 +0100

    feat(manage): add safe delete to delete attachment middleware

--- apps/manage/src/app/views/cases/view/manage-reps/task-list/attachment/delete-attachment-middleware.js
+++ apps/manage/src/app/views/cases/view/manage-reps/task-list/attachment/delete-attachment-middleware.js
@@ -1,3 +1,5 @@
+import { safeDeleteUploadedFilesSession } from '../../review/controller.js';
+
 export function buildDeleteRepresentationRedactedDocumentMiddleware(journeyId) {
 	return async (req, res, next) => {
 		if (journeyId === 'manage-reps-manage') {
@@ -6,12 +8,18 @@ export function buildDeleteRepresentationRedactedDocumentMiddleware(journeyId) {
 			if (!representationRef) {
 				throw new Error('representationRef param not found');
 			}
+			const itemId = params.itemId;
+			if (!itemId) {
+				throw new Error('itemId param not found');
+			}
 			const documentId = params.documentId;
 			if (!documentId) {
 				throw new Error('documentId param not found');
 			}
 
+			safeDeleteUploadedFilesSession(req, representationRef, itemId);
 			req.session?.itemsToBeDeleted?.[representationRef]?.push(params.documentId);
+
 			return res.redirect(req.baseUrl + '/redact');
 		}
 		return next();
