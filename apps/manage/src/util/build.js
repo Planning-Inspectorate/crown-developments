393775b65e0b1c279131410a545657b4fc3e62a7
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-07-23 13:48:51 +0100

    feat(manage): use moj library for sortable table

--- apps/manage/src/util/build.js
+++ apps/manage/src/util/build.js
@@ -12,11 +12,19 @@ async function run() {
 	const require = createRequire(import.meta.url);
 	// resolves to <root>/node_modules/govuk-frontend/dist/govuk/all.bundle.js than maps to `<root>`
 	const govUkRoot = path.resolve(require.resolve('govuk-frontend'), '../../../../..');
+	// resolves to <root>/node_modules/@ministryofjustice/frontend/moj/all.bundle.js than maps to `<root>`
+	const mojRoot = path.resolve(require.resolve('@ministryofjustice/frontend'), '../../../../..');
 	// resolves to <root>/node_modules/accessible-autocomplete/dist/*.js than maps to `dist`
 	const accessibleAutocompleteRoot = path.resolve(require.resolve('accessible-autocomplete'), '..');
 
 	const config = loadBuildConfig();
-	await runBuild({ staticDir: config.staticDir, srcDir: config.srcDir, govUkRoot, accessibleAutocompleteRoot });
+	await runBuild({
+		staticDir: config.staticDir,
+		srcDir: config.srcDir,
+		govUkRoot,
+		mojRoot,
+		accessibleAutocompleteRoot
+	});
 }
 
 // run the build, and write any errors to console
