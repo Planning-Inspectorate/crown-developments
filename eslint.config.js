9d4f40b139a5d7568396c518544e2d4545741aa6
Alex Moulin <87317681+AlexMoulin@users.noreply.github.com>
2025-08-27 15:43:07 +0100

    chore(deps): add client folder to gitignore, update import for PrismaClient, and add ignore to lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,10 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 export default [
-	js.configs.recommended,
+	{
+		...js.configs.recommended,
+		ignores: ['packages/database/src/client/**', '**/.static/**']
+	},
 	{
 		ignores: ['dist/**', 'node_modules/**', '**/*.min*.js', '**/static/scripts/app.js'],
 		languageOptions: {
--- packages/database/src/index.js
+++ packages/database/src/index.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@pins/crowndev-database/src/client/client.js';
 
 /** @typedef {import('@prisma/client').Prisma.PrismaClientOptions} prismaConfig */
 
