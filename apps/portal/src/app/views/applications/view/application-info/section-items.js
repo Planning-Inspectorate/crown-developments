7705fb3e5b086049380299feed4cf8cff5b95b2b
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-09-12 16:39:42 +0100

    feat(portal): move application info towards using summary items

---
+++ apps/portal/src/app/views/applications/view/application-info/section-items.js
@@ -0,0 +1,232 @@
+export function getAboutThisApplicationSectionItems(baseUrl, crownDevelopmentFields) {
+	return [
+		{
+			key: {
+				text: 'Application type'
+			},
+			value: {
+				text: crownDevelopmentFields.applicationType
+			}
+		},
+		{
+			key: {
+				text: 'Local planning authority'
+			},
+			value: {
+				text: crownDevelopmentFields.lpaName
+			}
+		},
+		{
+			key: {
+				text: 'Applicant name'
+			},
+			value: {
+				text: crownDevelopmentFields.applicantName
+			}
+		},
+		{
+			key: {
+				text: 'Site address'
+			},
+			value: {
+				text:
+					crownDevelopmentFields.siteAddress ||
+					`Easting: ${crownDevelopmentFields.siteCoordinates?.easting}, Northing: ${crownDevelopmentFields.siteCoordinates?.northing}`
+			}
+		},
+		{
+			key: {
+				text: 'Description of the proposed development'
+			},
+			value: {
+				text: crownDevelopmentFields.description
+			}
+		},
+		...(crownDevelopmentFields.stage
+			? [
+					{
+						key: {
+							text: 'Application Stage'
+						},
+						value: {
+							text: crownDevelopmentFields.stage
+						}
+					}
+				]
+			: []),
+		{
+			key: {
+				text: 'Application form'
+			},
+			value: {
+				html: `<p class="govuk-body">To view the full application, go to the <a class="govuk-link govuk-link--no-visited-state" href="${baseUrl}/documents">Documents</a> section.</p>`
+			}
+		}
+	];
+}
+
+export function getImportantDatesSectionItems(shouldShowImportantDatesSection, crownDevelopmentFields) {
+	if (!shouldShowImportantDatesSection) {
+		return [];
+	}
+	return [
+		...(crownDevelopmentFields.applicationAcceptedDate
+			? [
+					{
+						key: {
+							text: 'Application accepted date'
+						},
+						value: {
+							text: crownDevelopmentFields.applicationAcceptedDate
+						}
+					}
+				]
+			: []),
+		...(crownDevelopmentFields.representationsPeriodStartDateTime &&
+		crownDevelopmentFields.representationsPeriodEndDateTime
+			? [
+					{
+						key: {
+							text: 'Representation period'
+						},
+						value: {
+							text: `${crownDevelopmentFields.representationsPeriodStartDateTime} to ${crownDevelopmentFields.representationsPeriodEndDateTime}`
+						}
+					}
+				]
+			: []),
+		...(crownDevelopmentFields.decisionDate
+			? [
+					{
+						key: {
+							text: 'Decision date'
+						},
+						value: {
+							text: crownDevelopmentFields.decisionDate
+						}
+					}
+				]
+			: [])
+	];
+}
+
+export function getProcedureDetailsSectionItems(shouldShowProcedureDetailsSection, crownDevelopmentFields) {
+	if (!shouldShowProcedureDetailsSection) {
+		return [];
+	}
+
+	return [
+		{
+			key: {
+				text: 'Procedure type'
+			},
+			value: {
+				text: crownDevelopmentFields.procedure
+			}
+		},
+		...(crownDevelopmentFields.isInquiry && crownDevelopmentFields.inquiryStatementsDate
+			? [
+					{
+						key: {
+							text: 'Inquiry statements date'
+						},
+						value: {
+							text: crownDevelopmentFields.inquiryStatementsDate
+						}
+					}
+				]
+			: []),
+		...(crownDevelopmentFields.isInquiry && crownDevelopmentFields.inquiryDate
+			? [
+					{
+						key: {
+							text: 'Inquiry date'
+						},
+						value: {
+							text: crownDevelopmentFields.inquiryDate
+						}
+					}
+				]
+			: []),
+		...(crownDevelopmentFields.isInquiry && crownDevelopmentFields.inquiryVenue
+			? [
+					{
+						key: {
+							text: 'Inquiry venue'
+						},
+						value: {
+							text: crownDevelopmentFields.inquiryVenue
+						}
+					}
+				]
+			: []),
+		...(crownDevelopmentFields.isInquiry && crownDevelopmentFields.inquiryProofsOfEvidenceDate
+			? [
+					{
+						key: {
+							text: 'Inquiry proofs of evidence date'
+						},
+						value: {
+							text: crownDevelopmentFields.inquiryProofsOfEvidenceDate
+						}
+					}
+				]
+			: []),
+		...(crownDevelopmentFields.isHearing && crownDevelopmentFields.hearingDate
+			? [
+					{
+						key: {
+							text: 'Hearing date'
+						},
+						value: {
+							text: crownDevelopmentFields.hearingDate
+						}
+					}
+				]
+			: []),
+		...(crownDevelopmentFields.isHearing && crownDevelopmentFields.hearingVenue
+			? [
+					{
+						key: {
+							text: 'Hearing venue'
+						},
+						value: {
+							text: crownDevelopmentFields.hearingVenue
+						}
+					}
+				]
+			: [])
+	];
+}
+
+export function getApplicationDecisionSectionItems(shouldShowApplicationDecisionSection, crownDevelopmentFields) {
+	if (!shouldShowApplicationDecisionSection) {
+		return [];
+	}
+	return [
+		...(crownDevelopmentFields.decisionDate
+			? [
+					{
+						key: {
+							text: 'Decision date'
+						},
+						value: {
+							text: crownDevelopmentFields.decisionDate
+						}
+					}
+				]
+			: []),
+		...(crownDevelopmentFields.decisionOutcome
+			? [
+					{
+						key: {
+							text: 'Decision outcome'
+						},
+						value: {
+							text: crownDevelopmentFields.decisionOutcome
+						}
+					}
+				]
+			: [])
+	];
+}
