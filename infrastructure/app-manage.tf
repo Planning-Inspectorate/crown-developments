83a688e2492d1dbeb6a705b1c932fa7211e48016
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2025-10-23 13:37:20 +0100

    chore(infrastructure): testing tags in training env

--- infrastructure/app-manage.tf
+++ infrastructure/app-manage.tf
@@ -1,6 +1,6 @@
 module "app_manage" {
-  #checkov:skip=CKV_TF_1: Use of commit hash are not required for our Terraform modules
-  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=1.53"
+  #checkov:skip=CKV_TF_1: Explicit commit pin for reproducibility
+  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=54f5edd269367ebf4447ad444ae450c575bcde01"
 
   resource_group_name = azurerm_resource_group.primary.name
   location            = module.primary_region.location
