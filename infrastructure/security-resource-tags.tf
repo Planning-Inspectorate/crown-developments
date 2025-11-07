163168ed4d360163b01abe7f06a1388e0f6f968e
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2025-11-07 10:37:56 +0000

    chore(infrastructure): changing values of app

--- infrastructure/security-resource-tags.tf
+++ infrastructure/security-resource-tags.tf
@@ -5,24 +5,3 @@ module "security_tags" {
   base_tags  = local.tags
   extra_tags = each.value
 }
-
-locals {
-  resource_tags = {
-    manage = {
-      CriticalityRating = "Level 100"
-      PersonalData      = "Yes"
-    }
-    sql = {
-      CriticalityRating = "Level 3"
-      PersonalData      = "No"
-    }
-    portal = {
-      CriticalityRating = "Level 2"
-      PersonalData      = "Yes"
-    }
-    monitoring = {
-      CriticalityRating = "Level 99"
-      PersonalData      = "No"
-    }
-  }
-}
