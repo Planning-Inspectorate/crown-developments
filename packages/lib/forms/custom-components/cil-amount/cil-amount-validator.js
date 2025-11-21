b473d1a93dec3543c359856f670c4186f5bf8899
Oscar Gay <oscar.gay@solirius.com>
2025-11-21 17:20:15 +0000

    chore: adds a clarifying jsdoc for the class CILAmountValidator

--- packages/lib/forms/custom-components/cil-amount/cil-amount-validator.js
+++ packages/lib/forms/custom-components/cil-amount/cil-amount-validator.js
@@ -1,5 +1,12 @@
 import MonetaryInputValidator from '../monetary-input/monetary-input-validator.js';
 
+/**
+ * Community Infrastructure Levy (CIL) requires both a boolean value
+ * (where it is liable for it) and a numeric input (the amount libale).
+ *
+ * This validates that we have both those things (either yes and input,
+ * or no and no input) and displays the relevant text below if invalid.
+ */
 export default class CILAmountValidator extends MonetaryInputValidator {
 	constructor() {
 		super();
