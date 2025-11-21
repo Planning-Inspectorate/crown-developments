a2752307f84bf54cd39ac9d617186cac4b893239
Oscar Gay <oscar.gay@solirius.com>
2025-11-21 17:10:35 +0000

    feat: splits out cil into two fields but maintains the single component

--- apps/manage/src/app/views/cases/view/journey.js
+++ apps/manage/src/app/views/cases/view/journey.js
@@ -36,6 +36,7 @@ export function createJourney(questions, response, req) {
 		) &&
 		(questionHasAnswer(response, questions.subTypeOfApplication, APPLICATION_SUB_TYPE_ID.PLANNING_PERMISSION) ||
 			questionHasAnswer(response, questions.subTypeOfApplication, APPLICATION_SUB_TYPE_ID.LISTED_BUILDING_CONSENT));
+	const iscilLiable = (response) => yesNoToBoolean(response.answers?.cilLiable);
 
 	return new Journey({
 		journeyId: JOURNEY_ID,
@@ -72,6 +73,8 @@ export function createJourney(questions, response, req) {
 				.addQuestion(questions.siteIsVisibleFromPublicLand)
 				.addQuestion(questions.healthAndSafetyIssue)
 				.addQuestion(questions.cilLiable)
+				.addQuestion(questions.cilAmount)
+				.withCondition(iscilLiable)
 				.addQuestion(questions.bngExempt)
 				.addQuestion(questions.hasCostsApplications),
 			new Section('Contacts', 'contacts')
--- packages/lib/forms/custom-components/cil-amount/question.js
+++ packages/lib/forms/custom-components/cil-amount/question.js
@@ -1,4 +1,5 @@
 import MonetaryInputQuestion from '../monetary-input/question.js';
+import { capitalize } from '@planning-inspectorate/dynamic-forms/src/lib/utils.js';
 
 export default class CILAmountQuestion extends MonetaryInputQuestion {
 	constructor(props) {
@@ -7,5 +8,27 @@ export default class CILAmountQuestion extends MonetaryInputQuestion {
 			conditionalAmountFieldName: props.cilAmountInputFieldName,
 			conditionalAmountQuestion: props.cilAmountQuestion
 		});
+
+		this.fieldToShow = props.fieldToShow;
+	}
+
+	/**
+	 * Conditionally formats either the field (the boolean choice)
+	 * Or the conditionalInputField (the text input) - for showing
+	 * in the view case display.
+	 * @type {Question['formatAnswerForSummary']}
+	 */
+	formatAnswerForSummary(sectionSegment, journey, answer) {
+		if (this.fieldToShow === this.conditionalAmountFieldName) {
+			return super.formatAnswerForSummary(sectionSegment, journey, answer);
+		}
+
+		return [
+			{
+				key: this.title,
+				value: capitalize(journey.response.answers[this.fieldName]) || '-',
+				action: this.getAction(sectionSegment, journey, answer)
+			}
+		];
 	}
 }
