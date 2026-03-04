70159b9eacb26005e5ef381309a24d1e87b9955d
Steve Brett <Steve.Brett.04@planninginspectorate.gov.uk>
2026-03-04 09:06:03 +0000

    feat(manage): change applicationFee type to number and remove unused personalisation key

--- packages/lib/govnotify/types.d.ts
+++ packages/lib/govnotify/types.d.ts
@@ -42,7 +42,6 @@ export interface LpaAcknowledgeReceiptOfQuestionnairePersonalisation extends Com
 }
 
 export interface ApplicationReceivedDatePersonalisation extends CommonNotificationPersonalisation {
-	feeAmount: number;
 	applicationReceivedDate: string;
 	fee: string;
 }
