08385f85e40883055477033742e46cfa280ad9e9
Steve Brett <Steve.Brett.04@planninginspectorate.gov.uk>
2026-04-21 10:59:22 +0100

    feat(lib): remove deprecated rebrand assets

--- apps/manage/src/app/app.js
+++ apps/manage/src/app/app.js
@@ -69,7 +69,6 @@ export function getApp(service) {
 	const nunjucksEnvironment = configureNunjucks();
 	// Set the express view engine to nunjucks
 	// calls to res.render will use nunjucks
-	nunjucksEnvironment.addGlobal('govukRebrand', true);
 	nunjucksEnvironment.express(app);
 	app.set('view engine', 'njk');
 
