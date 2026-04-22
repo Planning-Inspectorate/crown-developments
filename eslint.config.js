139a21a74ada6da6218f8bc7c3e664b2ad309c71
Steve Brett <Steve.Brett.04@planninginspectorate.gov.uk>
2026-04-22 14:14:03 +0100

    fix(tooling): update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import checkFile from 'eslint-plugin-check-file';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -16,7 +17,6 @@ export default defineConfig([
 	...tseslint.configs.recommendedTypeChecked.map((config) => ({
 		...config,
 		files: ['**/*.ts'],
-		ignores: ['**/*.d.ts'],
 		languageOptions: {
 			parser: tseslint.parser,
 			parserOptions: {
@@ -35,8 +35,33 @@ export default defineConfig([
 	]),
 	eslintConfigPrettier,
 	{
+		files: ['**/*.{js,ts}'],
+		plugins: {
+			'check-file': checkFile
+		},
+		rules: {
+			'check-file/filename-naming-convention': [
+				'error',
+				{
+					'**/*.{js,ts}': 'KEBAB_CASE'
+				},
+				{
+					ignoreMiddleExtensions: true
+				}
+			],
+			'check-file/folder-naming-convention': [
+				'error',
+				{
+					'**/*': 'KEBAB_CASE'
+				}
+			]
+		}
+	},
+	{
 		files: ['**/*.ts'],
-		ignores: ['**/*.d.ts'],
+		plugins: {
+			'@typescript-eslint': tseslint.plugin
+		},
 		rules: {
 			'@typescript-eslint/consistent-type-imports': 'error'
 		}
