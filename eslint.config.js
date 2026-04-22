b3778fd8ee09e84ec7ca9311edf4c69b753febee
Steve Brett <Steve.Brett.04@planninginspectorate.gov.uk>
2026-04-22 12:45:44 +0100

    feat(tooling): add typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,44 @@
 import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default defineConfig([
+	js.configs.recommended,
 	{
-		...js.configs.recommended,
-		ignores: ['packages/database/src/client/**', '**/.static/**']
-	},
-	{
-		ignores: ['dist/**', 'node_modules/**', '**/*.min*.js', '**/static/scripts/app.js'],
 		languageOptions: {
 			ecmaVersion: 2025,
 			sourceType: 'module',
-			globals: {
-				...globals.node
-			}
+			globals: globals.node
 		}
 	},
-	eslintConfigPrettier
-];
+	...tseslint.configs.recommendedTypeChecked.map((config) => ({
+		...config,
+		files: ['**/*.ts'],
+		ignores: ['**/*.d.ts'],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname
+			}
+		}
+	})),
+	globalIgnores([
+		'.husky',
+		'dist/**',
+		'node_modules/**',
+		'**/*.test.ts',
+		'**/.static/**',
+		'packages/database/src/client/**'
+	]),
+	eslintConfigPrettier,
+	{
+		files: ['**/*.ts'],
+		ignores: ['**/*.d.ts'],
+		rules: {
+			'@typescript-eslint/consistent-type-imports': 'error'
+		}
+	}
+]);
