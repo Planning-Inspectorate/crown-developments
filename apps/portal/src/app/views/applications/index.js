6caa7855ac23b22a261df5b85d135f9465b92948
Alex Moulin <alex.moulin@planninginspectorate.gov.uk>
2026-04-23 16:37:17 +0100

    fix(portal): remove caching middleware from non-document pages

--- apps/portal/src/app/views/applications/index.js
+++ apps/portal/src/app/views/applications/index.js
@@ -2,7 +2,6 @@ import { Router as createRouter } from 'express';
 import { createRoutes as createViewRoutes } from './view/index.js';
 import { buildApplicationListPage } from './list/controller.js';
 import { asyncHandler } from '@pins/crowndev-lib/util/async-handler.js';
-import { buildCachingDynamicContentMiddleware } from '../util/caching-middleware.js';
 
 /**
  * @param {import('#service').PortalService} service
@@ -12,9 +11,8 @@ export function createRoutes(service) {
 	const router = createRouter({ mergeParams: true });
 
 	const applicationListController = buildApplicationListPage(service);
-	const cachingMiddleware = buildCachingDynamicContentMiddleware(service);
 
-	router.get('/applications', cachingMiddleware, asyncHandler(applicationListController));
+	router.get('/applications', asyncHandler(applicationListController));
 	router.use('/applications/:applicationId', createViewRoutes(service));
 
 	return router;
