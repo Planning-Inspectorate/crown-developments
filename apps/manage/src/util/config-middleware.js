a3cd26daf9c300f340ec00b472d23e49ca5f1f56
andrewlilley-solirius <andrew.lilley@solirius.com>
2026-06-30 14:24:25 +0100

    fix(manage): addition of section 62a header for back office service (PEAS-242)

--- apps/manage/src/util/config-middleware.js
+++ apps/manage/src/util/config-middleware.js
@@ -5,9 +5,51 @@
  */
 export function addLocalsConfiguration({ appName }) {
 	return (req, res, next) => {
+		const path = req.path;
+
+		const s62aLinks = [
+			{
+				text: 'Home',
+				href: '/s62a/cases'
+			},
+			{
+				text: 'Create new case',
+				href: '/s62a/cases/create-a-case/pre-application-or-application'
+			},
+			{
+				text: 'Sign out',
+				href: '/s62a/auth/signout'
+			}
+		];
+
+		const crownLinks = [
+			{
+				text: 'All Cases',
+				href: '/crown/cases'
+			},
+			{
+				text: 'Sign out',
+				href: '/auth/signout'
+			}
+		];
+
+		const links = res.locals.isS62A ? s62aLinks : crownLinks;
+
+		const headerTitle = res.locals.isS62A
+			? 'Manage Section 62A applications'
+			: 'Manage a Crown Development Application';
+
 		res.locals.config = {
-			appName
+			appName,
+			headerTitle: headerTitle,
+			isLive: true,
+			inBeta: false,
+			primaryNavigationLinks: links.map((link) => ({
+				...link,
+				current: link.href === path
+			}))
 		};
+
 		next();
 	};
 }
