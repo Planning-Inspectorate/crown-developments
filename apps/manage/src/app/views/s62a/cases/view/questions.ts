e2510975332510f9c208308d7247c9961ff38c47
Oscar Gay <oscar.gay@solirius.com>
2026-07-23 16:30:42 +0100

    chore: fixes tests and type errors

--- apps/manage/src/app/views/s62a/cases/view/questions.ts
+++ apps/manage/src/app/views/s62a/cases/view/questions.ts
@@ -20,6 +20,7 @@ import {
 } from '@pins/crowndev-database/src/seed/s62a/data-static.ts';
 import {
 	AddressValidator,
+	BOOLEAN_OPTIONS,
 	COMPONENT_TYPES,
 	CoordinatesValidator,
 	createQuestions,
@@ -81,10 +82,16 @@ export function getQuestions(answers: S62aCaseViewModel, isQuestionView?: boolea
 		validators: [new CILAmountValidator(), new CILAmountLengthValidator()]
 	};
 
+	// TODO: Make sure to remove these error expectations.
+	// @ts-expect-error - the view model update is coming in next PR
 	const isIndividual = answers?.applicantType === APPLICANT_TYPE_ID.INDIVIDUAL;
-
+	// @ts-expect-error - the view model update is coming in next PR
 	const manageApplicantOrganisations = !isIndividual ? (answers?.manageApplicantOrganisations as ApplicantOrg[]) : [];
 	const applicantOrganisationOptions = getApplicantOrganisationOptions(manageApplicantOrganisations);
+	// @ts-expect-error - the view model update is coming in next PR
+	const hasAgent = answers?.hasAgent === BOOLEAN_OPTIONS.YES;
+
+	const applicantContactsValidator = getApplicantContactsValidator(hasAgent, isIndividual);
 
 	const questions = {
 		reference: {
@@ -1224,7 +1231,7 @@ export function getQuestions(answers: S62aCaseViewModel, isQuestionView?: boolea
 			showAnswersInSummary: true,
 			maximumAnswers: 10,
 			emptyStateAddStyle: 'prominent',
-			validators: getApplicantContactsValidator
+			validators: applicantContactsValidator
 		},
 		...multiContactQuestions({
 			prefix: 'applicant',
