7300ad0fbc056f99128287f69d940873d2dc9cee
Oscar Gay <oscar.gay@solirius.com>
2026-07-24 11:43:23 +0100

    feat: adds the view model functionality for contacts

--- apps/manage/src/app/views/s62a/cases/view/index.ts
+++ apps/manage/src/app/views/s62a/cases/view/index.ts
@@ -7,23 +7,26 @@ import {
 	buildGetJourneyResponseFromSession,
 	buildSave,
 	question,
+	saveDataToSession,
 	validate,
 	validationErrorHandler
 } from '@planning-inspectorate/dynamic-forms';
 import { JOURNEY_ID } from './journey.ts';
 import { buildS62aUpdateCase } from './update-case.ts';
+import { buildDeleteS62aManageListItemOnConfirmRemove } from './delete.ts';
 
 export function createRoutes(service: ManageService) {
 	const router = createRouter({ mergeParams: true });
 
-	const getJourney = asyncHandler(buildGetJourneyMiddleware(service));
+	const getJourney = asyncHandler(buildGetJourneyMiddleware(service, false));
 	const viewCaseDetails = buildViewCaseDetails();
-	const getQuestionJourney = asyncHandler(buildGetJourneyMiddleware(service));
+	const getQuestionJourney = asyncHandler(buildGetJourneyMiddleware(service, true));
 	const updateCaseFn = buildS62aUpdateCase(service);
 	const updateCase = buildSave(updateCaseFn, true);
 	const clearAndUpdateCaseFn = buildS62aUpdateCase(service, true);
 	const clearAndUpdateCase = buildSave(clearAndUpdateCaseFn, true);
 	const getJourneyResponse = buildGetJourneyResponseFromSession(JOURNEY_ID);
+	const deleteManageListItemOnConfirmRemove = asyncHandler(buildDeleteS62aManageListItemOnConfirmRemove(service));
 
 	router.get('/', (req, res) => {
 		res.redirect(`${req.baseUrl}/${VIEW_TAB_ID.OVERVIEW}`);
@@ -34,7 +37,12 @@ export function createRoutes(service: ManageService) {
 	const tabRouter = createRouter({ mergeParams: true });
 	tabRouter.get('/', getJourney, asyncHandler(viewCaseDetails));
 
-	tabRouter.get('/:section/:question', getJourneyResponse, getQuestionJourney, asyncHandler(question));
+	tabRouter.get(
+		'/:section/:question{/:manageListAction/:manageListItemId/:manageListQuestion}',
+		getJourneyResponse,
+		getQuestionJourney,
+		asyncHandler(question)
+	);
 
 	tabRouter.post(
 		'/:section/:question',
@@ -43,6 +51,16 @@ export function createRoutes(service: ManageService) {
 		validate,
 		validationErrorHandler,
 		asyncHandler(updateCase)
+	);
+
+	tabRouter.post(
+		'/:section/:question{/:manageListAction/:manageListItemId/:manageListQuestion}',
+		getJourneyResponse,
+		getQuestionJourney,
+		validate,
+		validationErrorHandler,
+		deleteManageListItemOnConfirmRemove,
+		buildSave(saveDataToSession)
 	);
 
 	tabRouter.post('/:section/:question/remove', getJourney, asyncHandler(clearAndUpdateCase));
