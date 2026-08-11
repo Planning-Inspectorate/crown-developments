ea59ed6559e4f1fc29b980e3b70d3ff6296cb176
Steve Brett <Steve.Brett.04@planninginspectorate.gov.uk>
2026-08-11 17:17:12 +0100

    refactor(lib): convert boolean util to typescript

--- packages/lib/util/boolean.ts
+++ packages/lib/util/boolean.ts
@@ -1,10 +1,7 @@
 /**
  * Use filter(isDefined) instead of filter(Boolean) to help TypeScript
  * understand that falsy values have been filtered out.
- * @template T
- * @param {T} value
- * @returns {value is Exclude<T, false | '' | 0 | null | undefined>}
  */
-export function isDefined(value) {
+export function isDefined<T>(value: T): value is Exclude<T, false | '' | 0 | null | undefined> {
 	return Boolean(value);
 }
