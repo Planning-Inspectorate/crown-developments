09e51779c550bc87d195c629dbc6e1803e68a8e8
Alex Moulin <alex.moulin@planninginspectorate.gov.uk>
2026-08-12 11:08:01 +0100

    fix(front-door): update managed rules for Microsoft DRS

--- infrastructure/front-door-manage.tf
+++ infrastructure/front-door-manage.tf
@@ -136,41 +136,70 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "manage" {
     }
   }
 
+
+  #############################################################################
+  # MANAGED RULES - Microsoft Default Rule Set
+  #############################################################################
+
   managed_rule {
     type    = "Microsoft_DefaultRuleSet"
     version = "2.1"
     action  = "Block"
 
+    #--------------------------------------------------------------------------
+    # General (200xxx)
+    #--------------------------------------------------------------------------
     override {
-      rule_group_name = "PROTOCOL-ATTACK"
+      rule_group_name = "General"
+      # General: Failed to parse request body (5PL1)
       rule {
         action  = "Log"
-        rule_id = "921110"
+        rule_id = "200002"
         enabled = true
       }
-      exclusion {
-        match_variable = "RequestBodyPostArgNames"
-        operator       = "Equals"
-        selector       = "files"
+      # General: Multipart request body failed strict validation (5PL1)
+      rule {
+        action  = "Log"
+        rule_id = "200003"
+        enabled = true
       }
-      exclusion {
-        match_variable = "RequestBodyPostArgNames"
-        operator       = "Equals"
-        selector       = "_csrf"
-      }
-
     }
 
+    #--------------------------------------------------------------------------
+    # PROTOCOL-ENFORCEMENT (920xxx)
+    #--------------------------------------------------------------------------
     override {
       rule_group_name = "PROTOCOL-ENFORCEMENT"
+      # Protocol Violation: URL Encoding Abuse Attack Attempt
       rule {
         action  = "AnomalyScoring"
         rule_id = "920240"
         enabled = true
-
         exclusion {
-          # 920240 - Protocol Violation: URL Encoding Abuse Attack Attempt
-          # PostParamValue:_csrf","matchVariableValue":"..."}]
+          match_variable = "RequestBodyPostArgNames"
+          operator       = "Equals"
+          selector       = "_csrf"
+          # False positive: PostParamValue:_csrf","matchVariableValue":"..."}]
+        }
+      }
+    }
+
+    #--------------------------------------------------------------------------
+    # PROTOCOL-ATTACK (921xxx)
+    #--------------------------------------------------------------------------
+    override {
+      rule_group_name = "PROTOCOL-ATTACK"
+      # HTTP Request Smuggling Attack (5PL1)
+      rule {
+        action  = "Log"
+        rule_id = "921110"
+        enabled = true
+        exclusion {
+          match_variable = "RequestBodyPostArgNames"
+          operator       = "Equals"
+          selector       = "files"
+        }
+        exclusion {
           match_variable = "RequestBodyPostArgNames"
           operator       = "Equals"
           selector       = "_csrf"
@@ -178,47 +207,65 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "manage" {
       }
     }
 
+    #--------------------------------------------------------------------------
+    # XSS - Cross-site scripting (941xxx)
+    #--------------------------------------------------------------------------
     override {
       rule_group_name = "XSS"
+      # JavaScript global variable found (5PL1)
       rule {
         action  = "AnomalyScoring"
         rule_id = "941370"
         enabled = true
 
         exclusion {
-          # 941370 - XSS Attack: JavaScript global variable found
-          # PostParamValue:submitterComment","matchVariableValue":"...to object to..."}]
           match_variable = "RequestBodyPostArgNames"
           operator       = "Equals"
           selector       = "submitterComment"
+          # PostParamValue:submitterComment","matchVariableValue":"...to object to..."}]
         }
       }
     }
 
-
-
+    #--------------------------------------------------------------------------
+    # SQLI - SQL Injection (942xxx)
+    #--------------------------------------------------------------------------
     override {
       rule_group_name = "SQLI"
+      # SQL Operator Detected
+      rule {
+        action  = "Log"
+        enabled = true
+        rule_id = "942120"
+      }
+      # SQL Injection Attack
       rule {
         action  = "Log"
         rule_id = "942390"
         enabled = true
       }
-      rule {
-        # SQL Injection Attack: SQL Operator Detected
-        action  = "Log"
-        enabled = true
-        rule_id = "942120"
+
+      # Group-level exclusions (alphabetical by selector)
+      exclusion {
+        match_variable = "RequestBodyPostArgNames"
+        operator       = "Equals"
+        selector       = "_csrf"
       }
       exclusion {
         match_variable = "RequestBodyPostArgNames"
         operator       = "Equals"
-        selector       = "myselfComment"
+        selector       = "comment"
+      }
+      exclusion {
+        match_variable = "RequestCookieNames"
+        operator       = "Equals"
+        selector       = "connect.sid"
       }
       exclusion {
         match_variable = "RequestBodyPostArgNames"
         operator       = "Equals"
-        selector       = "submitterComment"
+        selector       = "details"
+        # application update details
       }
       exclusion {
         match_variable = "RequestBodyPostArgNames"
@@ -228,8 +275,27 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "manage" {
       exclusion {
         match_variable = "RequestBodyPostArgNames"
         operator       = "Equals"
-        selector       = "comment"
+        selector       = "myselfComment"
       }
+      exclusion {
+        match_variable = "RequestBodyPostArgNames"
+        operator       = "Equals"
+        selector       = "submitterComment"
+      }
+    }
+
+    #--------------------------------------------------------------------------
+    # MS Threat Intel SQLI (99031xxx)
+    #--------------------------------------------------------------------------
+    override {
+      rule_group_name = "MS-ThreatIntel-SQLI"
+      # SQL Injection Attack
+      rule {
+        action  = "Log"
+        rule_id = "99031003"
+        enabled = true
+      }
+      # Group-level exclusions (alphabetical by selector)
       exclusion {
         match_variable = "RequestBodyPostArgNames"
         operator       = "Equals"
@@ -240,14 +306,10 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "manage" {
         operator       = "Equals"
         selector       = "details" # application update details
       }
-    }
-
-    override {
-      rule_group_name = "MS-ThreatIntel-SQLI"
-      rule {
-        action  = "Log"
-        rule_id = "99031003"
-        enabled = true
+      exclusion {
+        match_variable = "RequestBodyPostArgNames"
+        operator       = "Equals"
+        selector       = "healthAndSafetyIssue"
       }
       exclusion {
         match_variable = "RequestBodyPostArgNames"
@@ -258,35 +320,6 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "manage" {
         match_variable = "RequestBodyPostArgNames"
         operator       = "Equals"
         selector       = "submitterComment"
-      }
-      exclusion {
-        match_variable = "RequestBodyPostArgNames"
-        operator       = "Equals"
-        selector       = "_csrf"
-      }
-      exclusion {
-        match_variable = "RequestBodyPostArgNames"
-        operator       = "Equals"
-        selector       = "healthAndSafetyIssue"
-      }
-      exclusion {
-        match_variable = "RequestBodyPostArgNames"
-        operator       = "Equals"
-        selector       = "details" # application update details
-      }
-    }
-
-    override {
-      rule_group_name = "General"
-      rule {
-        action  = "Log"
-        rule_id = "200002"
-        enabled = true
-      }
-      rule {
-        action  = "Log"
-        rule_id = "200003"
-        enabled = true
       }
     }
   }
