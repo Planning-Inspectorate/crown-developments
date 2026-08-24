5cb880ec4564a93cacb9e69e073f66a08546ed9f
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-08-24 15:20:03 +0100

    chore: use @planning-inspectorate/coding-standards

--- eslint.config.js
+++ eslint.config.js
@@ -1,68 +1,24 @@
-import js from '@eslint/js';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import checkFile from 'eslint-plugin-check-file';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import { eslintConfig } from '@planning-inspectorate/coding-standards';
 
 export default defineConfig([
-	js.configs.recommended,
+	...eslintConfig,
 	{
-		languageOptions: {
-			ecmaVersion: 2025,
-			sourceType: 'module',
-			globals: globals.node
-		}
-	},
-	...tseslint.configs.recommendedTypeChecked.map((config) => ({
-		...config,
 		files: ['**/*.ts'],
+		// use a more strict typescript ruleset
+		extends: [tseslint.configs.recommendedTypeChecked],
 		languageOptions: {
-			parser: tseslint.parser,
 			parserOptions: {
+				// requires type information from TypeScript
+				// https://typescript-eslint.io/getting-started/typed-linting
 				projectService: true,
 				tsconfigRootDir: import.meta.dirname
 			}
-		}
-	})),
-	globalIgnores([
-		'.husky',
-		'dist/**',
-		'node_modules/**',
-		'**/*.test.ts',
-		'**/.static/**',
-		'packages/database/src/client/**'
-	]),
-	eslintConfigPrettier,
-	{
-		files: ['**/*.{js,ts}'],
-		plugins: {
-			'check-file': checkFile
 		},
 		rules: {
-			'check-file/filename-naming-convention': [
-				'error',
-				{
-					'**/*.{js,ts}': 'KEBAB_CASE'
-				},
-				{
-					ignoreMiddleExtensions: true
-				}
-			],
-			'check-file/folder-naming-convention': [
-				'error',
-				{
-					'**/*': 'KEBAB_CASE'
-				}
-			]
-		}
-	},
-	{
-		files: ['**/*.ts'],
-		plugins: {
-			'@typescript-eslint': tseslint.plugin
-		},
-		rules: {
+			// add in the type imports rule as per coding-standards
+			// coding-standards also allows 'any', but we are more strict and leave that rule as default
 			'@typescript-eslint/consistent-type-imports': 'error'
 		}
 	}
